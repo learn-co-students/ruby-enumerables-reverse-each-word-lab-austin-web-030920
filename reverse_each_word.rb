ddefc13cf36e30686afbdb565b0ec1ec8bbed4d7
Joshua Gauthreaux <joshuagauthreaux@gmail.com>
2020-02-25 18:46:54 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,17 @@
 require 'pry' #binding.pry
 
 def reverse_each_word(string)
-new = string.split
-newnew= new.collect do |reverso|  
-  reverso.reverse 
-  end
-  return newnew.join(' ')
+run = string.split.collect do |reverso|
+  reverso.reverse
 end
+run.join(" ")
+end
+
+
+# def reverse_each_word(string)
+# new = string.split
+# newnew= new.collect do |reverso|  
+#   reverso.reverse 
+#   end
+#   return newnew.join(' ')
+# end
